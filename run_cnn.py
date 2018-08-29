1dd34d605155121ac9482c56981fddccf4741635
yeyifeng <banyip@gmail.com>
2018-08-29 12:56:38 +0800

    read catagory from file

--- run_cnn.py
+++ run_cnn.py
@@ -21,7 +21,7 @@ test_dir = os.path.join(base_dir, 'cnews.test.txt')
 val_dir = os.path.join(base_dir, 'cnews.val.txt')
 vocab_dir = os.path.join(base_dir, 'cnews.vocab.txt')
 category_dir = os.path.join(base_dir, 'cnews.keys.txt')
-categories, cat_to_id = read_category(category_dir)
+
 
 save_dir = 'checkpoints/textcnn'
 save_path = os.path.join(save_dir, 'best_validation')  # 最佳验证结果保存路径
@@ -191,7 +191,7 @@ if __name__ == '__main__':
     config = TCNNConfig()
     if not os.path.exists(vocab_dir):  # 如果不存在词汇表，重建
         build_vocab(train_dir, vocab_dir, config.vocab_size)
-    categories, cat_to_id = read_category()
+    categories, cat_to_id = read_category(category_dir)
     words, word_to_id = read_vocab(vocab_dir)
     config.vocab_size = len(words)
     model = TextCNN(config)
